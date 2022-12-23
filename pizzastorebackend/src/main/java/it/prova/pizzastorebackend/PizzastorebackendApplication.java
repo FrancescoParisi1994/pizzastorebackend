69eb7a85db4165fe200c6ce35be352b933dc1227
unknown <tridirendi@gmail.com>
2022-12-23 14:25:45 +0100

    inizio progetto

---
+++ pizzastorebackend/src/main/java/it/prova/pizzastorebackend/PizzastorebackendApplication.java
@@ -0,0 +1,13 @@
+package it.prova.pizzastorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastorebackendApplication.class, args);
+	}
+
+}
